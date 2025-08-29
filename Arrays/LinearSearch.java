8fffa657cf898cd9c96a19da6e1f4383d60af381
Vishesh Garg <63488278+gargvishesh9@users.noreply.github.com>
2025-08-29 11:39:07 +0530

    LinearSearch.java

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,28 @@
+
+
+```public class Main {
+    //Time Complexity -> O(n)
+    //Since we run this program n times searching for every element in array 
+    public static void main(String[] args) {
+
+        String [] menu = {"Burger","Pizza","Pasta","Momos"};
+        String item = "Momos";
+        int index = linearSearch(menu,item);
+
+        if(index==-1){
+            System.out.println("Item is not present in the menu");
+        }else{
+            System.out.println("Item is present at index " +index);
+        }
+    }
+
+    public static int linearSearch(String [] menu,String item){
+
+        for(int i=0;i<menu.length;i++){
+            if(menu[i].equals(item)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
